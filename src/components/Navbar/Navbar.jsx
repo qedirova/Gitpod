0ec607f074f7b5317c3b4d30c6cd1eec4c39b74f
Abbas Ibragimov <abbasibragimov123@gmail.com>
2025-08-29 23:22:21 +0400

    fixes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,12 @@
-import React from "react";
 import s from "./style.module.scss";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
+  const navLinks = [
+    { id: 1, title: "Home", href: "/" },
+    { id: 2, title: "Pricing", href: "/pricing" },
+    { id: 3, title: "Contact", href: "/contact" },
+  ];
   return (
     <nav>
       <div className="container">
@@ -10,6 +14,16 @@ export default function Navbar() {
           <Link to={"/"}>
             <img src="/Logo.svg" alt="logo" />
           </Link>
+
+          <div className={s.links}>
+            {navLinks.map((link) => (
+              <Link key={link.id} className={s.link} to={link.href}>
+                {link.title}
+              </Link>
+            ))}
+          </div>
+
+          <button className={s.button}>Contact</button>
         </div>
       </div>
     </nav>
