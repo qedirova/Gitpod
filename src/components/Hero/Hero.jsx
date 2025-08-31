3f6cfc71b76ab0a3709c48ba5e37968c4fd66c0a
Abbas Ibragimov <abbasibragimov123@gmail.com>
2025-08-31 13:33:20 +0400

    Fix bugs in hero and create a mobile version

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,27 +2,29 @@ import s from "./style.module.scss";
 
 export default function Hero() {
   return (
-    <div>
-      <div className={s.container}>
-        <div className={s.wrapper}>
-          <div className={s.text}>
-            <h1>Always Ready to Code.</h1>
+    <div className="container">
+      <div className={s.wrapper}>
+        <div className={s.text}>
+          <h1>
+            Always <br /> Ready to Code.
+          </h1>
 
-            <p className={s.p1}>
-              Spin up fresh, automated dev environments for each task, in the
-              cloud, in seconds.
+          <p className={s.paragraph}>
+            Spin up fresh, automated dev environments for each task, in the
+            cloud, in seconds.
+          </p>
+          <div className={s.text1}>
+            <button className={s.button}>Try Now</button>
+
+            <p className={s.p}>
+              Open a workspace.
+              <br />
+              Start from any Git context.
             </p>
-            <div className={s.text1}>
-              <button className={s.button}>Try Now</button>
-
-              <p className={s.p}>
-                Open a workspace.Start from any Git context.
-              </p>
-            </div>
           </div>
-
-          <img src="Hero.png" alt="hero" />
         </div>
+
+        <img className={s.img} src="Hero.png" alt="hero" />
       </div>
     </div>
   );
