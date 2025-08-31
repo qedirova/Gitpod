7cbcb68d5d81ed38d3e121aa3f7bad05d268295f
Banu Qedirova <qedirovabanu0@gmail.com>
2025-08-31 13:48:31 +0400

    create steps section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Hero from "./components/Hero/Hero";
+import Steps from "./components/Steps/Steps";
 
 function App() {
   return (
-    <div>
+    <>
       <Hero />
-    </div>
+      <Steps />
+    </>
   );
 }
 
