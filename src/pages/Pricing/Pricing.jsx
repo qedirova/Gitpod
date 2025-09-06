0e8ecdeabc3f07ef4856614df3a5db38864d7999
Banu Qedirova <qedirovabanu0@gmail.com>
2025-09-06 21:06:09 +0400

    create pricing page

--- src/pages/Pricing/Pricing.jsx
+++ src/pages/Pricing/Pricing.jsx
@@ -2,5 +2,35 @@ import React from "react";
 import s from "./style.module.scss";
 
 export default function Pricing() {
-  return <div>Pricing</div>;
+  return (
+    <div className="container">
+      <div className={s.main}>
+        <div className={s.text}>
+          <h1>Pricing</h1>
+          <p>Get more Gitpod, pay less.</p>
+        </div>
+        <button className={s.btn}>Try now</button>
+      </div>
+      <div className={s.pricing}>
+        <div className={s.card}>
+          <h2>Sign up to be the first to receive matches.</h2>
+
+          <div className={s.option}>
+            <div>1 month</div>
+            <div>9.99$</div>
+          </div>
+          <div className={s.option}>
+            <div>12 month</div>
+            <div>99.99$</div>
+          </div>
+          <div className={s.option}>
+            <div>24 month</div>
+            <div>199.99$</div>
+          </div>
+
+          <button className={s.btnPurchase}>Purchase now</button>
+        </div>
+      </div>
+    </div>
+  );
 }
